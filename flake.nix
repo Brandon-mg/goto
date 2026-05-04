7a6268f2f4308b130696e02d145b1c1346bfddfb
Brandon-mg <reachbmg@gmail.com>
2026-05-04 16:25:30 -0700

    update flake

--- flake.nix
+++ flake.nix
@@ -8,9 +8,13 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    goto-src = {
+      url = "github:Brandon-mg/goto";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, goto-src }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
@@ -24,8 +28,8 @@
           default = pkgs.rustPlatform.buildRustPackage {
             pname = "goto";
             version = "0.1.0";
-            src = ./.;
-            cargoLock.lockFile = ./Cargo.lock;
+            src = goto-src;
+            cargoLock.lockFile = "${goto-src}/Cargo.lock";
             nativeBuildInputs = [ rustToolchain ];
             meta = {
               description = "jump to directories";
